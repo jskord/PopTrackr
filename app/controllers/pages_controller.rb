815d22ba9ccc0f88b127fc9b74a44a9ccd1bb692
Justin Skord <jskord@gmail.com>
2019-04-20 10:03:49 -0700

    added link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,7 @@ class PagesController < ApplicationController
   def index
     render 'index.html.erb'
   end
+  def cable
+    render 'cable.html.erb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
 
   get '/' => 'pages#index'
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
+  get '/cable' => 'pages#cable'
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
