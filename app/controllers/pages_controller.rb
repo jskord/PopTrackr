5f0b0ee933541f46f7d780226bafb5ed621a01ad
Justin Skord <jskord@gmail.com>
2019-04-24 00:41:07 -0500

    graphwork

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,10 @@ class PagesController < ApplicationController
     @x_axis = []
     ecoli_level = Unirest.get('https://data.cityofchicago.org/api/views/t62e-8nvc/rows.json?accessType=DOWNLOAD').body
     ecoli_level["data"].each do |level|
-      @y_axis << level[10] 
+      @y_axis << level[10].to_i 
+    end
+    ecoli_level["data"].each do |level|
+      @x_axis << Date.parse(level[9])
     end
 
     render 'cable.html.erb'
