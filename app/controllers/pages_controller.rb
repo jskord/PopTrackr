3005fb33edccd9b3daab2fc191aa1c36ae14c363
Justin Skord <jskord@gmail.com>
2019-04-25 01:00:44 -0500

    graph works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,16 +5,17 @@ class PagesController < ApplicationController
   end
   def one
     @y_axis = []
-    @x_axis = []
+    @data = []
     @series = []
     ecoli_level = Unirest.get('https://data.cityofchicago.org/api/views/t62e-8nvc/rows.json?accessType=DOWNLOAD').body
     ecoli_level["data"].each do |level|
       @y_axis << level[10].to_i 
     end
     ecoli_level["data"].each do |level|
-      date = level[9].to_date
-      formatted_date = date.strftime("%m/%d/%Y")
-      @x_axis << formatted_date
+      date = level[9].to_time
+      date_utc = date.to_f * 1000
+      ecoli_concentration = level[10].to_i
+      @data << [date_utc, ecoli_concentration]
     end
 
     render 'cable.html.erb'
