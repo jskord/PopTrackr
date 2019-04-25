7ca41355a2c87b780aed6822ca9709509511d43f
Justin Skord <jskord@gmail.com>
2019-04-24 23:00:37 -0500

    strftime shit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,12 +6,15 @@ class PagesController < ApplicationController
   def one
     @y_axis = []
     @x_axis = []
+    @series = []
     ecoli_level = Unirest.get('https://data.cityofchicago.org/api/views/t62e-8nvc/rows.json?accessType=DOWNLOAD').body
     ecoli_level["data"].each do |level|
       @y_axis << level[10].to_i 
     end
     ecoli_level["data"].each do |level|
-      @x_axis << Date.parse(level[9])
+      date = level[9].to_date
+      formatted_date = date.strftime("%m/%d/%Y")
+      @x_axis << formatted_date
     end
 
     render 'cable.html.erb'
